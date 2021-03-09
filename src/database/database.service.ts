c1e063080f333d3adbc12813923e8408b88fb7c2
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2021-03-09 13:08:30 +0100

    chore: typeorm and config

---
+++ src/database/database.service.ts
@@ -0,0 +1,27 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import { ConnectionOptions } from 'typeorm';
+import { Configuration } from '../config/config.keys';
+
+export const databaseProviders = [
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    async useFactory(config: ConfigService) {
+      return {
+        // ssl: true,
+        type: 'postgres' as 'postgres',
+        host: process.env.HOST || config.get(Configuration.HOST),
+        port: 5444,
+        database: process.env.DATABASE || config.get(Configuration.DATABASE),
+        username: process.env.USERNAME || config.get(Configuration.USERNAME),
+        password: process.env.PASSWORD || config.get(Configuration.PASSWORD),
+        synchronize: false,
+        logging: true,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/{.ts,.js}'],
+      } as ConnectionOptions;
+    },
+  }),
+];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  await app.listen(AppModule.port, '0.0.0.0');
+  logger.log(`Application listening on port ${AppModule.port}`);
 }
 bootstrap();
